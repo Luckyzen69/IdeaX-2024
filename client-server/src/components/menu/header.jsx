a6a2e021f0f8ff1417891fc46095efa14ba55c77
Lagzen Thakuri <thakurizen2@gmail.com>
2024-09-27 16:19:18 +0545

    "Updated .gitignore, package-lock.json, and package.json files; added new dependencies and peer dependencies; modified App.jsx, header.jsx, and tailwind.config.js files"

--- client-server/src/components/menu/header.jsx
+++ client-server/src/components/menu/header.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import Logo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <>
-    <div>
-        <div>
+    <div className='flex justify-between items-center m-2 mr-20 '>
+        <div className='flex items-center'>
             <img src={Logo} alt="" />
-            <h6>Agro-Sikshya</h6>
+            <h6 className='text-xl'>Agro-Sikshya</h6> 
         </div>
 
-        <ul>
-            <li>home</li>
-            <li>course  </li>
-            <li>login </li>
-            <li>register </li>
+        <ul className='flex space-x-10 capitalize cursor-pointer'>
+            <Link to='/'><li className='m-3 hover:text-accent'>home</li></Link>
+            <Link to='/course'><li className='m-3 hover:text-accent'>course  </li></Link>
+            <Link to='/'><li className='m-3 hover:text-accent'>login </li></Link>
+            <Link to='/'><li className='border rounded-md m-1  px-4 py-2 bg-accent hover:bg-fourth text-white'>register </li></Link>
         </ul>
     </div>
     </>
